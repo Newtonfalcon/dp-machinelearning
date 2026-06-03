c0820992466636428c5a147b1c699c5b7565d00d
Newton <newtonoyagha@gmail.com>
2026-06-03 23:01:06 +0100

    Update CSV URL to use raw content link

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,5 +5,5 @@ st.title('machine learning app')
 
 st.info('this is a machine learning app')
 
-df = pd.read_csv("https://github.com/dataprofessor/data/blob/master/penguins_cleaned.csv")
+df = pd.read_csv("https://raw.githubusercontent.com/dataprofessor/data/refs/heads/master/penguins_cleaned.csv")
 df
